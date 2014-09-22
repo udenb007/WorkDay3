56cbb6b1161924febcaccea42bad0d3bdf947891
idsc3101 <idsc3101@gmail.com>
2014-09-22 17:12:04 -0500

    Workday #3

--- IfElse/IfElse/Form1.vb
+++ IfElse/IfElse/Form1.vb
@@ -5,14 +5,24 @@ Public Class Form1
     Private Sub btnFindLarger_Click(sender As System.Object, e As System.EventArgs) Handles btnFindLarger.Click
         Dim num1, num2, largerNum As Double
 
-        ' Modify the program such that it does not crash 
+        ' Modify the program such that it does not crash
         ' if the user types incorrect input
 
         Dim st1 As String = txtFirstNum.Text
         Dim st2 As String = txtSecondNum.Text
 
+        If Not (IsNumeric(st1) And IsNumeric(st2)) Then
+            MessageBox.Show("Please type the numbers")
+            Return
+        End If
+
         num1 = CDbl(st1)
         num2 = CDbl(st2)
+
+        If Not (num1 >= 0 And num2 >= 0) Then
+            MessageBox.Show("Please type positive numbers")
+            Return
+        End If
 
         Dim result As String
 
@@ -25,6 +35,7 @@ Public Class Form1
         End If
 
         txtResult.Text = result
+
     End Sub
 
 End Class
